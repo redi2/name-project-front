74752a7ccc472422e0fec4a370544673743a4677
bethel <you@example.com>
2020-09-22 00:05:40 -0400

    fixing the title found in the home page is done

--- src/App.js
+++ src/App.js
@@ -1,92 +1,93 @@
 /**
  * App.js fro the main routing and basic layout
- * 
+ *
  * @author Practical IT
  */
-import React, { Fragment, useState } from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React, { Fragment, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import AddMoreDetail from './components/name/more-detail';
-import LandingParent from './components/landing/landing-parent';
+import AddMoreDetail from "./components/name/more-detail";
+import LandingParent from "./components/landing/landing-parent";
 
 function App() {
   const routes = [
     {
-      path: '/',
-      component: LandingParent
+      path: "/",
+      component: LandingParent,
     },
     {
-      path: '/more-detail',
-      component: AddMoreDetail
-    } 
+      path: "/more-detail",
+      component: AddMoreDetail,
+    },
   ];
 
   return (
     <Router>
-    <Fragment>
+      <Fragment>
+        <nav className="navbar navbar-light bg-light static-top">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              The NAME (Heritage) Project
+            </a>
+            <a className="btn btn-primary" href="#">
+              Volunteer
+            </a>
+          </div>
+        </nav>
 
-      <nav className="navbar navbar-light bg-light static-top">
-        <div className="container">
-          <a className="navbar-brand" href="#">The NAME - Heritage - Project</a>
-          <a className="btn btn-primary" href="#">Volunteer</a>
-        </div>
-      </nav>
-      
-      {routes.map( ({path, component}) => {
-        return (<Route
-          path={path} exact
-          component={component}
-          />);
-      })}
-      <footer className="footer bg-light">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 h-100 text-center text-lg-left my-auto">
-              <ul className="list-inline mb-2">
-                <li className="list-inline-item">
-                  <a href="#">About</a>
-                </li>
-                <li className="list-inline-item">&sdot;</li>
-                <li className="list-inline-item">
-                  <a href="#">Contact</a>
-                </li>
-                <li className="list-inline-item">&sdot;</li>
-                <li className="list-inline-item">
-                  <a href="#">Terms of Use</a>
-                </li>
-                <li className="list-inline-item">&sdot;</li>
-                <li className="list-inline-item">
-                  <a href="#">Privacy Policy</a>
-                </li>
-              </ul>
-              <p className="text-muted small mb-4 mb-lg-0">&copy; ThePracticalIT. All Rights Reserved.</p>
-            </div>
-            <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
-              <ul className="list-inline mb-0">
-                <li className="list-inline-item mr-3">
-                  <a href="#">
-                    <i className="fab fa-facebook fa-2x fa-fw"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item mr-3">
-                  <a href="#">
-                    <i className="fab fa-twitter-square fa-2x fa-fw"></i>
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a href="#">
-                    <i className="fab fa-instagram fa-2x fa-fw"></i>
-                  </a>
-                </li>
-              </ul>
+        {routes.map(({ path, component }) => {
+          return <Route path={path} exact component={component} />;
+        })}
+        <footer className="footer bg-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 h-100 text-center text-lg-left my-auto">
+                <ul className="list-inline mb-2">
+                  <li className="list-inline-item">
+                    <a href="#">About</a>
+                  </li>
+                  <li className="list-inline-item">&sdot;</li>
+                  <li className="list-inline-item">
+                    <a href="#">Contact</a>
+                  </li>
+                  <li className="list-inline-item">&sdot;</li>
+                  <li className="list-inline-item">
+                    <a href="#">Terms of Use</a>
+                  </li>
+                  <li className="list-inline-item">&sdot;</li>
+                  <li className="list-inline-item">
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                </ul>
+                <p className="text-muted small mb-4 mb-lg-0">
+                  &copy; ThePracticalIT. All Rights Reserved.
+                </p>
+              </div>
+              <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
+                <ul className="list-inline mb-0">
+                  <li className="list-inline-item mr-3">
+                    <a href="#">
+                      <i className="fab fa-facebook fa-2x fa-fw"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item mr-3">
+                    <a href="#">
+                      <i className="fab fa-twitter-square fa-2x fa-fw"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a href="#">
+                      <i className="fab fa-instagram fa-2x fa-fw"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
-
-    </Fragment>
+        </footer>
+      </Fragment>
     </Router>
   );
 }
